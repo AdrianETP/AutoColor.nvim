f3117d89a2050c4e6479e3744505935968c44939
Adrian Eduardo Treviño Peña <eduardotrevinop@yahoo.com.mx>
2022-12-31 17:49:04 -0600

    changeColor

--- lua/autocolor/init.lua
+++ lua/autocolor/init.lua
@@ -1,5 +1,13 @@
 local M = {}
+vim.cmd [[let g:colors = getcompletion('','color')]]
 
+function ChangeColor(values)
+    vim.cmd("colorscheme " .. values.theme)
+    if values.transparent then
+        vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+        vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+    end
+end
 function M.setup(config)
 
 end
